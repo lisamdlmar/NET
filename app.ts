e4cf93cb689129feea13fbd4c98793207410eeda
Lisam dlmar <hysumaql@Hysums-MBP.fios-router.home>
2019-09-14 19:27:57 -0400

    Added bodyParser

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser'
 
 const app = express();
+
 const port = 3000;
+
+// parse application/json 
+app.use(bodyParser.json())
+
+
 app.get('/', (req, res) => {
   res.send('Hello from / server side !! : )');
 });
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
