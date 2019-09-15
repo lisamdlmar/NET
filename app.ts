0c9bc932ad2a657d5a396fae9ade110c9b6dd2a5
Lisam dlmar <hysumaql@Hysums-MBP.fios-router.home>
2019-09-14 20:19:12 -0400

    Added Express routes

--- app.ts
+++ app.ts
@@ -1,21 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import morgan from 'morgan';
+import API from './ controllers/api'
 
 const app = express();
-
+const router = express.Router();
 const port = 3000;
 
+
 // parse application/json 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // create "middleware"
 app.use(morgan('combined'))
+// Router
+app.use(router);
 
 
-app.get('/', (req, res) => {
-  res.send('Hello from / server side !! : )');
+router.get('/', (req, res) => {
+ return res.send('Hello from / server side !! : )');
 });
 
+
+router.post('/api/v1/parse',API.APIV1);
+//router.post('/api/v2/parse',controller.APIV2);
+
+   
 app.listen(port, err => {
   if (err) {
     return console.error(err);
